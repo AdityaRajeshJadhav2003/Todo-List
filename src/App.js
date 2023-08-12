1fa8fe8aa15c5ebb2a739e3b7480c668acba0a97
Aditya Jadhav <adityajadhav1582@gmail.com>
2023-08-12 11:22:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Todolist from './components/todolist';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <Todolist/>
+
+    </div>
+  );
+}
+
+export default App;
